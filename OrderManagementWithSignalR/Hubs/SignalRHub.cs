8495d0c1d7ea445cd096be4d1ab74d39a602fb57
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-11-25 10:17:51 +0300

    add sendcategorycount, sendproductcount to siganlrhub

--- OrderManagementWithSignalR/Hubs/SignalRHub.cs
+++ OrderManagementWithSignalR/Hubs/SignalRHub.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Concrete;
+﻿using Business.Abstract;
+using DataAccess.Concrete;
 using Microsoft.AspNetCore.SignalR;
 
 namespace WebApi.Hubs
@@ -6,12 +7,32 @@ namespace WebApi.Hubs
 	public class SignalRHub:Hub
 	{
 		//Contructor kullanmadıgımız için program.cs te configurasyon yapmadık
-		SignalRDbContext context = new SignalRDbContext();
+		//SignalRDbContext context = new SignalRDbContext();
 
+		private readonly ICategoryService _categoryService;
+		private readonly IProductService _productService;
+
+        public SignalRHub(ICategoryService categoryService, IProductService productService)
+        {
+            _categoryService = categoryService;
+			_productService = productService;
+        }
+
+		// Asenkron bir metodun, tamamlandığında bir sonuç döndürmesi gerekmez.
+		// Eğer döndürmüyorsa, metot Task türünde olur.
+		// Eğer sonuç döndürüyorsa, Task<T> türünde olur
 		public async Task SendCategoryCount()
 		{
-			var value = context.Categories.Count();
+			//var value = context.Categories.Count();
+			var value = _categoryService.CategoryCount();
 			await Clients.All.SendAsync("ReceiveCategoryCount", value);
+		}
+
+		public async Task SendProductCount()
+		{
+			//var value = context.Categories.Count();
+			var value = _productService.ProductCount();
+			await Clients.All.SendAsync("ReceiveProductCount", value);
 		}
 
 	}
