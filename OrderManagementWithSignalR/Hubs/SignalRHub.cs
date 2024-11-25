e8b21d45435b9a9185d8b1246c79586ace727392
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-11-25 16:32:41 +0300

    added components

--- OrderManagementWithSignalR/Hubs/SignalRHub.cs
+++ OrderManagementWithSignalR/Hubs/SignalRHub.cs
@@ -4,19 +4,26 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace WebApi.Hubs
 {
-	public class SignalRHub:Hub
+	public class SignalRHub : Hub
 	{
 		//Contructor kullanmadıgımız için program.cs te configurasyon yapmadık
 		//SignalRDbContext context = new SignalRDbContext();
 
 		private readonly ICategoryService _categoryService;
 		private readonly IProductService _productService;
+		private readonly IMoneyCaseService _moneyCaseService;
+		private readonly IOrderService _orderService;
+		private readonly IMenuTableService _menuTableService;
 
-        public SignalRHub(ICategoryService categoryService, IProductService productService)
-        {
-            _categoryService = categoryService;
+		public SignalRHub(ICategoryService categoryService, IProductService productService,
+			IMoneyCaseService moneyCaseService, IOrderService orderService, IMenuTableService menuTableService)
+		{
+			_categoryService = categoryService;
 			_productService = productService;
-        }
+			_moneyCaseService = moneyCaseService;
+			_orderService = orderService;	
+			_menuTableService = menuTableService;
+		}
 
 		// Asenkron bir metodun, tamamlandığında bir sonuç döndürmesi gerekmez.
 		// Eğer döndürmüyorsa, metot Task türünde olur.
@@ -32,12 +39,32 @@ namespace WebApi.Hubs
 			await Clients.All.SendAsync("ReceiveProductCount", productCount);
 
 			var activeCategoryCount = _categoryService.ActiveCategoryCount();
-			await Clients.All.SendAsync("RecieveActiveCategoryCount ", activeCategoryCount);
+			await Clients.All.SendAsync("RecieveActiveCategoryCount", activeCategoryCount);
+			Console.WriteLine("test");
+			Console.WriteLine(activeCategoryCount);
 
 			var passiveCategoryCount = _categoryService.PassiveCategoryCount();
-			await Clients.All.SendAsync("RecievePassiveCategoryCount ", passiveCategoryCount);
+			await Clients.All.SendAsync("RecievePassiveCategoryCount", passiveCategoryCount);
 		}
-		
 
+		public async Task SendProgressbar()
+		{
+			var moneyCaseAmount = _moneyCaseService.TotalMoneyCaseAmount();
+			await Clients.All.SendAsync("RecieveMoneyCaseAmount", moneyCaseAmount);
+			Console.WriteLine("test");
+			Console.WriteLine(moneyCaseAmount);
+
+			var activeOrderCount = _orderService.ActiveOrderCount();
+			await Clients.All.SendAsync("RecieveActiveOrderCount", activeOrderCount);
+			Console.WriteLine("test");
+			Console.WriteLine(activeOrderCount);
+
+			var menuTableCount = _menuTableService.MenuTableCount();
+			await Clients.All.SendAsync("RecieveMenuTableCount", menuTableCount);
+			Console.WriteLine("test");
+			Console.WriteLine(menuTableCount);
+
+
+		}
 	}
 }
