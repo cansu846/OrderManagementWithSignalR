18a3c3b282481b00658378225ef99e6e6d953a6e
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-11-28 19:47:34 +0300

    feat: implement Notification module with CRUD operations and database migration

---
+++ SignalRWebUI/Controllers/NotificationController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.NotificationDtos;
+using System.Text;
+
+namespace SignalRWebUI.Controllers
+{
+	public class NotificationController : Controller
+	{
+		IHttpClientFactory _httpClientFactory;
+
+		public NotificationController(IHttpClientFactory httpClientFactory)
+		{
+			_httpClientFactory = httpClientFactory;
+		}
+		public async Task<IActionResult> Index()
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7142/api/Notification");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultNotificationDto>> (jsonData);
+				return View(values);
+			}
+
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> CreateNotification()
+		{
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> CreateNotification(CreateNotificationDto createNotificationDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createNotificationDto);
+			StringContent stringContent = new StringContent(jsonData,Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7142/api/Notification", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateNotification(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync($"https://localhost:7142/api/Notification/id?id={id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var value = JsonConvert.DeserializeObject<UpdateNotificationDto>(jsonData);
+				return View(value);
+			}
+			return View();
+		}
+
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateNotification(UpdateNotificationDto updateNotificationDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateNotificationDto);
+			StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7142/api/Notification", stringContent);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+		public async Task<IActionResult> DeleteNotification(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			
+			var responseMessage = await client.DeleteAsync($"https://localhost:7142/api/Notification/{id}");
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
+			}
+			return View();
+		}
+
+        public async Task<IActionResult> NotificationStatusChangeToTrue(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.GetAsync($"https://localhost:7142/api/Notification/NotificationStatusChangeToTrue/{id}");
+            return RedirectToAction("Index");
+        }
+
+        public async Task<IActionResult> NotificationStatusChangeToFalse(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            await client.GetAsync($"https://localhost:7142/api/Notification/NotificationStatusChangeToFalse/{id}");
+            return RedirectToAction("Index");
+        }
+    }
+		
+}
