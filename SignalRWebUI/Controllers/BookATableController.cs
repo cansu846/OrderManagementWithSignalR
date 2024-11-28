d0690d2411efdd87fdf68d9ca636d65e853d03cd
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-11-28 11:51:25 +0300

    feat: Add BookATable feature with controller and view

---
+++ SignalRWebUI/Controllers/BookATableController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace SignalRWebUI.Controllers
+{
+    public class BookATableController:Controller
+    {
+        IHttpClientFactory _httpClientFactory;
+
+        public BookATableController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> Index()
+        {
+
+            return View();
+        }
+        
+    }
+}
